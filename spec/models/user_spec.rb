95ec8f6c2180afd11ff357253030bf994bab1512
masarakki <masaki@hisme.net>
2014-06-26 05:24:48 +0900

    add nested_attributes test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,6 +6,17 @@ describe User do
     it { expect(build(:user)).to be_valid }
   end
 
+  describe "nested_attributes_for" do
+    let(:user) {
+      User.create(
+        first_names_attributes: [{locale: 'ja', value: 'ジョン'}, {locale: 'en', value: 'John'}],
+        last_names_attributes: [{locale: 'ja', value: 'レノン'}, {locale: 'en', value: 'Lennon'}]
+        )
+    }
+    it { expect(user.first_names.count).to eq 2 }
+    it { expect(user.last_names.count).to eq 2 }
+  end
+
   describe "translatable" do
     before { I18n.available_locales = [:en, :ja] }
     let(:user) { create :user }
