cde6bf003a1bc97ec4befd58297886d6d16187f1
masarakki <masaki@hisme.net>
2014-06-28 01:01:46 +0900

    null guard

--- lib/translatable/acts_as_translatable.rb
+++ lib/translatable/acts_as_translatable.rb
@@ -11,7 +11,7 @@ module Translatable
         :inverse_of => :translatable, :dependent => :destroy
         accepts_nested_attributes_for key_name
         define_method column do |locale = I18n.default_locale|
-          send(key_name).detect{|w| w.locale.to_s == locale.to_s }.value
+          send(key_name).detect{|w| w.locale.to_s == locale.to_s }.try(:value)
         end
       end
     end
