8a85145c964f8dabbdcee568cfc80bd422f9bcf1
masarakki <masaki@hisme.net>
2014-11-01 02:29:34 +0900

    fix code style

--- lib/translatable/acts_as_translatable.rb
+++ lib/translatable/acts_as_translatable.rb
@@ -4,15 +4,23 @@ module Translatable
   module ActsAsTranslatable
     extend ActiveSupport::Concern
 
+    included do
+
+      private
+
+      def translated_word(key_name, locale)
+        localizes = send(key_name)
+        (localizes.find { |w| w.locale.to_s == locale.to_s } || localizes.first).try(:value)
+      end
+    end
+
     module ClassMethods
       def translatable(column)
         key_name = column.to_s.pluralize.to_sym
-        has_many key_name, -> { where(key: column.to_s) }, as: :translatable, class_name: 'TranslatedWord',
-        inverse_of: :translatable, dependent: :destroy
+        has_many key_name, -> { where(key: column.to_s) }, as: :translatable, class_name: 'TranslatedWord', inverse_of: :translatable, dependent: :destroy
         accepts_nested_attributes_for key_name
         define_method column do |locale = I18n.default_locale|
-          localizes = send(key_name)
-          localizes.find { |w| w.locale.to_s == locale.to_s }.try(:value) || localizes.first.try(:value)
+          translated_word(key_name, locale)
         end
       end
     end
